bb6afecd9128e54bce7aa2c5ca91448460da0c23
Manash2005 <swainm099@gmail.com>
2026-02-19 06:46:13 +0530

    Connected backend with client side

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 const authRoutes = require("./routes/auth.routes")
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
+app.use(cors({
+    origin : "http://localhost:5000/api",
+    credentials : true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
